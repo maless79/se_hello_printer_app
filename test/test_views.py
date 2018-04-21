10575c1ee44664a35f2b5b7f3c3f96547d28ca92
maless79 <33197542+maless79@users.noreply.github.com>
2018-04-21 13:03:47 +0200

    Update test_views.py

--- test/test_views.py
+++ test/test_views.py
@@ -14,4 +14,4 @@ class FlaskrTestCase(unittest.TestCase):
 
     def test_msg_with_output(self):
         rv = self.app.get('/?output=json')
-        self.assertEquals('{ "imie":"ADOLF", "mgs":"Hello World!"}', rv.data)
+        self.assertEquals('{ "imie":"KAROL", "mgs":"Hello World!"}', rv.data)
